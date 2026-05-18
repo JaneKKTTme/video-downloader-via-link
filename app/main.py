c02f782659f15a800122bf8d5a3c66c0040844cf
Jane Khomenko <eva.khomenko.28@mail.ru>
2026-05-19 00:57:08 +0300

    Feat(templates): add Jinja2 templating engine

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from urllib.parse import unquote
+
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
 
 from app.api.v1 import health
 
@@ -11,10 +13,31 @@ app = FastAPI(
 	version='1.0.0'
 )
 
+templates = Jinja2Templates(directory='app/templates')
+
 app.include_router(health.router, tags=['health'])
 
 app.mount('/static', StaticFiles(directory='app/static'), name='static')
 
+
 @app.get('/')
-async def root():
-	return FileResponse('app/templates/video_downloader_page.html')
+async def root(
+	request: Request,
+	link: str = None,
+	result: bool = False,
+	message: str = None,
+	file_path: str = None
+):
+	if link:
+		link = unquote(link)
+	if message:
+		message = unquote(message)
+		
+	context_data = {
+		'request': request,
+		'link': link,
+		'result': result, 
+		'message': message,
+		'file_path': file_path
+	}
+	return templates.TemplateResponse('video_downloader_page.html', context_data)
